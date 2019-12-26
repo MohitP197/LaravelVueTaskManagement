c42c18d1648aa33f1f6ce2fa9bc262672146ec0e
Mohit Paudel <39890575+MohitP197@users.noreply.github.com>
2019-12-26 07:19:29 +0545

    Added Navbar and Tasks component

---
+++ resources/js/components/Partials/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav class="navbar navbar-expand-sm navbar-dark bg-primary mb-2">
+      <div class="container">
+          <a href="#" class="navbar-brand">Task Management</a>
+      </div>
+  </nav>
+</template>
---
+++ resources/js/components/Tasks/Tasks.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <h2>Tasks</h2>
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li v-bind:class="[{disabled: !pagination.prev_page_url}]" class="page-item">
+          <a class="page-link" href="#" @click="fetchTasks(pagination.prev_page_url)">Previous</a>
+        </li>
+        <li v-bind:class="[{disabled: !pagination.next_page_url}]" class="page-item">
+          <a class="page-link" href="#" @click="fetchTasks(pagination.next_page_url)">Next</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="card card-body mb-2" v-for="task in tasks" v-bind:key="task.id">
+      <h3>{{task.title}}</h3>
+      <h3>{{task.description}}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tasks: [],
+      task: {
+        id: "",
+        title: "",
+        description: "",
+        status: ""
+      },
+      task_id: "",
+      pagination: {},
+      edit: false
+    };
+  },
+  created() {
+    this.fetchTasks();
+  },
+  methods: {
+    fetchTasks(page_url) {
+      let vm = this;
+      page_url = page_url || "/api/task";
+      fetch(page_url)
+        .then(res => res.json())
+        .then(res => {
+          this.tasks = res.data;
+          vm.makePagination(res.meta, res.links);
+        })
+        .catch(err => console.log(err));
+    },
+    makePagination(meta, links) {
+      let pagination = {
+        current_page: meta.current_page,
+        last_page: meta.last_page,
+        next_page_url: links.next,
+        prev_page_url: links.prev
+      };
+      this.pagination = pagination;
+    }
+  }
+};
+</script>
